ef85f7a8ecfd18c4103749c3300be6329f12cc9b
Raffaello <raffaello@erg.abdn.ac.uk>
2021-06-16 15:43:27 +0100

    Added file with traces names

--- run_ML_test.sh
+++ run_ML_test.sh
@@ -3,10 +3,28 @@
 BINSIZE_MS=1000
 NUM_BINS=10
 
-# Ground Truth Label 0
-./udprate ../QUIC_traces/RTP-google-meet.pcapng $BINSIZE_MS $NUM_BINS 326 -1 0 > GT.csv
 
-# Ground Truth Label 1
-./udprate ../QUIC_traces/youtube+web.pcpng.pcapng $BINSIZE_MS $NUM_BINS 551 630 1 | \
- awk 'NR>1' >> GT.csv
+trap 'rm -f "$TMP0" "$TMP1" "$TMP2"' EXIT
+TMP0=$(mktemp) || exit 1
+TMP1=$(mktemp) || exit 1
+TMP2=$(mktemp) || exit 1
+
+cat filenames |\
+while read fname start end label cmt
+do
+
+	if [ -z $fname ]; then continue; fi
+	if [ $fname = "#" ]; then continue; fi
+
+	echo ./udprate $fname $BINSIZE_MS $NUM_BINS $start $end $label 
+	./udprate $fname $BINSIZE_MS $NUM_BINS $start $end $label > $TMP0
+
+	awk 'NR==1' $TMP0 > $TMP1
+	awk 'NR>1'  $TMP0 >> $TMP2
+		
+done
+
+cat $TMP1 $TMP2 > GT.csv
+
+
 
