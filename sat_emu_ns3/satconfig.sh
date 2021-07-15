f6ab707f19e60906770250c20ca50e96adff567b
root <Raffaello Secchi>
2021-07-15 10:15:47 +0100

    Some instructions

--- sat_emu_ns3/satconfig.sh
+++ sat_emu_ns3/satconfig.sh
@@ -3,6 +3,8 @@
 # Name on the local interface on public network
 LOCAL_ETH=enp0s3
 
+# ns-3 directory 
+NS3_DIR=/root/workspace/SatnexV_WP21/ns-allinone-3.33/ns-3.33/
 
 # Creates permanent TAP for ns-3
 tunctl -t tap-left
@@ -23,7 +25,7 @@ ip route add 10.1.1.0/24 via 10.1.2.2
 iptables -t nat -A POSTROUTING -o $LOCAL_ETH -j MASQUERADE 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-cd /root/workspace/SatnexV_WP21/ns-allinone-3.33/ns-3.33/
+cd $NS3_DIR 
 ./waf --run satdelay
 
 
