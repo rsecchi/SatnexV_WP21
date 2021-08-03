f3d383eb773dd80dd8754881cdb136ba25164ec0
Raffaello <raffaello@erg.abdn.ac.uk>
2021-08-03 19:05:36 +0100

    gzip added to check_boundaries.sh

--- check_boundaries.sh
+++ check_boundaries.sh
@@ -2,7 +2,15 @@
 
 FILE=${1:?"$0 <filename.pcap>"}
 
-./udprate $FILE 1000 1 0 -1 0 > out.log
+if [ ${FILE: -3} == ".gz" ]; then
+	TMPFILE=$(mktemp -u)
+	gzip -d $FILE  --stdout > $TMPFILE
+	./udprate $TMPFILE 1000 1 0 -1 0 > out.log
+	rm $TMPFILE
+else
+	./udprate $file 1000 1 0 -1 0 > out.log
+fi
+
   
 echo "plot 'out.log' u 0:1 w l" | gnuplot -p
 
