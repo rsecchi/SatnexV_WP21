a721cc6863c5a3fe84bd855c2c7acd41ec0e0e5b
Raffaello Secchi <r.secchi@gmail.com>
2021-08-05 13:50:46 +0100

    netem configuration

--- sat_emu_ns3/netem_config.sh
+++ sat_emu_ns3/netem_config.sh
@@ -7,11 +7,19 @@ LOCAL_TUN=tun0
 
 DELAY=200ms         # average delay
 DELAY_VAR=10ms      # uniform distribution
-BANDWIDTH=2Mbit
+BANDWIDTH=2Mbit     # downlink bandwidth
+QLEN=50             # buffer size in packets
+
+
+#############################
+
+ifconfig $LOCAL_TUN txqueuelen $QLEN
 
 tc qdisc del dev $LOCAL_TUN root
-tc qdisc add dev $LOCAL_TUN root handle 1: tbf rate $BANDWIDTH  buffer 1600 limit 3000
-tc qdisc add dev $LOCAL_TUN parent 1:1 handle 10: netem delay $DELAY $DELAY_VAR
+tc qdisc add dev $LOCAL_TUN root tbf rate $BANDWIDTH  buffer 1600 limit 10000
+
+tc qdisc del dev $LOCAL_ETH root
+tc qdisc add dev $LOCAL_ETH root netem delay $DELAY $DELAYVAR
 
 iptables -t nat -F
 iptables -t nat -A POSTROUTING -o $LOCAL_ETH -j MASQUERADE 
